4c9e5e43e02726ce8defc5e5c68f50d67d2c2ea6
DESKTOP-DUFR1UE\user <pushik1@yandex.ru>
2017-11-16 16:56:04 +0300

    1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите первое число");
+
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+
+            Console.WriteLine("Введите второе число");
+            string S2 = Console.ReadLine();
+            int b = int.Parse(S2);
+
+            int c = a + b;
+
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
