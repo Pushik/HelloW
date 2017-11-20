ea6f41bfd41c35be86d18cdb80405afa2b7f36f2
DESKTOP-DUFR1UE\user <pushik1@yandex.ru>
2017-11-20 16:45:33 +0300

    20/11/2017

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,26 +1,44 @@
 ﻿using System;
-namespace HelloWorld
+using System.Net;
+using System.IO;
+
+	
+namespace Wex
 {
-    class Hello
+    class Wex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Введите первое число");
+            Console.WriteLine("Курс валюты с Wex");
+            String Url = "https://wex.nz/api/3/btc_usd";
+            
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(Url);
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+            StreamReader objReader = new StreamReader(objStream);
 
-            string S1 = Console.ReadLine();
-            int a = int.Parse(S1);
+            string sLine = "";
+            sLine = objReader.ReadLine();
+            Console.WriteLine(sLine);
 
-            Console.WriteLine("Введите второе число");
-            string S2 = Console.ReadLine();
-            int b = int.Parse(S2);
 
-            int c = a + b;
 
-            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
-            // Keep the console window open in debug mode.
+            //Console.WriteLine("Введите первое число");
+            //string S1 = Console.ReadLine();
+            //int a = int.Parse(S1);
+
+            //Console.WriteLine("Введите второе число");
+            //string S2 = Console.ReadLine();
+            //int b = int.Parse(S2);
+            //int c = a + b;
+            //Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
+
+
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
+
+       
     }
 }
